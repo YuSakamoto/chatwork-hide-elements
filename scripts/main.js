03a2b0415ff3992f28a20ee73a6f30d1a035294d
Zheng SHAO <z-shao@colopl.jp>
2016-02-20 00:11:03 +0900

    add pin-contact feature

--- scripts/main.js
+++ scripts/main.js
@@ -17,4 +17,37 @@
             $(hideElements).css("display","none");
         } , 1000);
     });
+
+	chrome.storage.sync.get("pinContact", function(model) {
+        if (model.pinContact == true){
+            console.log("pinContact start");
+
+            var blockTimer = setInterval(function(){
+                var contacts = [];
+                var others   = [];
+                $("#_roomListItems>li").each(function(){
+                    if ( $(this).hasClass("roomUnread") ){
+
+                        // contact
+                        if ( $("div>img", this).hasClass("_avatar") ){
+                            contacts.push( $(this) );
+                            return true;
+                        }
+                    }
+                    others.push( $(this) );
+                });
+                
+                if (contacts.length < 1) { return; }
+
+                $("#_roomListItems").empty();
+                $.each( contacts, function( index, value ){
+                    $("#_roomListItems").append( value );
+                });
+
+                $.each( others, function( index, value ){
+                    $("#_roomListItems").append( value );
+                });
+            } , 1000);
+        }
+	});        
 }());
