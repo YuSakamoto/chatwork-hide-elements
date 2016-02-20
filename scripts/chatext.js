cea7fa41c33446a496319441b21a6b0b3bbd3091
Zheng SHAO <axot@msn.com>
2016-02-20 20:43:50 +0900

    add mark all as read feature

---
+++ scripts/chatext.js
@@ -0,0 +1,30 @@
+(function () {
+	// mark all as read
+	// add a button
+	$(".sideContentMenu").append("<div id=\"_markButton\" role=\"button\" class=\"sideTitleBtn _button btnLarge _cwBN button\" style=\"right: 80px;\"><span class=\"_sideContentTitleText\">M</span> </div>");
+	
+	$("#_markButton").click(function(){
+		if(!confirm('本当に全部を既読してよろしいですか？')){
+			return false;
+		}else{
+			/*　OKの時の処理 */
+			// find all unread rooms
+			var unread_rooms = [];
+			$.each(RL.rooms, function(index, room){
+				if (room.getUnreadNum() > 0){
+					unread_rooms.push(room);
+				}
+			});
+			$.each(unread_rooms, function(index, room){
+				g = room.getViewStat();
+				RM && RM.id == room.id ? g = RM.getViewStat() : e = room.getUnreadNum();
+				CW.get("gateway.php", {
+					cmd: "read",
+					room_id: room.id,
+					mid: room.mid,
+					last_chat_id: g.last_read_id,
+				});
+			});
+		}
+	});
+}());
